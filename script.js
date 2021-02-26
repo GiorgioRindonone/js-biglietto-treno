4d915030ae687890bd2744e033591655a7754472
grindonone <giorgio.rindonone@gmail.com>
2021-02-26 17:28:12 +0100

    update_1

---
+++ script.js
@@ -0,0 +1,21 @@
+var eta = prompt("inserisci la tua età");
+
+var km = prompt("inserisci i chilometri che vuoi percorrere");
+
+var prezzoBigliettokm = 0.21;
+
+var costoBiglietto = km * prezzoBigliettokm;
+
+var bigliettoScontato = "";
+
+// calcolo lo sconto
+
+if (eta > 65) {
+  bigliettoScontato = costoBiglietto * 0.40;
+} else if (eta < 18){
+  bigliettoScontato = costoBiglietto * 0.20;
+} else {
+  bigliettoScontato = costoBiglietto ;
+}
+
+document.getElementById('prezzo-finale').innerHTML = "Dopo aver calcolato minuziosamente la sua età, le proponiamo questa offerta alla modica cifra di: " + costoBiglietto + " €";
