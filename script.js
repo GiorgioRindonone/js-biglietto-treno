3515bcfb87a1b3f119455d7963a2f51dc316e777
grindonone <giorgio.rindonone@gmail.com>
2021-02-26 17:55:56 +0100

    update_5

--- script.js
+++ script.js
@@ -6,28 +6,26 @@ var eta = prompt("inserisci la tua età");
 // var src = document.getElementById("header");
 // src.appendChild(img);
 
-if (parseInt(eta) == NaN) {
+if (parseInt(eta) == true) {
   eta = prompt("inserisci la tua età, non fare il biricchino!");
 
-} else {
-  var km = prompt("inserisci i chilometri che vuoi percorrere");
-
-  var prezzoBigliettokm = 0.21;
-
-  var costoBiglietto = km * prezzoBigliettokm;
-
-  var bigliettoScontato = "";
-
-  // calcolo lo sconto
-
-  if (eta > 65) {
-    bigliettoScontato = costoBiglietto - (costoBiglietto * 0.40);
-  } else if (eta < 18){
-    bigliettoScontato = costoBiglietto - (costoBiglietto * 0.20);
-  } else {
-    bigliettoScontato = costoBiglietto ;
-  }
 }
 
+var km = prompt("inserisci i chilometri che vuoi percorrere");
 
+var prezzoBigliettokm = 0.21;
+
+var costoBiglietto = km * prezzoBigliettokm;
+
+var bigliettoScontato = "";
+
+// calcolo lo sconto
+
+if (eta > 65) {
+  bigliettoScontato = costoBiglietto - (costoBiglietto * 0.40);
+} else if (eta < 18){
+  bigliettoScontato = costoBiglietto - (costoBiglietto * 0.20);
+} else {
+  bigliettoScontato = costoBiglietto ;
+}
 document.getElementById('prezzo-finale').innerHTML = "Dopo aver calcolato minuziosamente la sua età, le proponiamo questa offerta alla modica cifra di: " + costoBiglietto + " €";
